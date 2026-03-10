dad554f54d439b36640efb9b31d88d6c82d1aafb
idawy <a15516080597@qq.com>
2026-03-10 21:02:32 +0800

    安装Rust环境，创建cjson_rust项目，运行HelloWorld成功

---
+++ cjson_rust/target/debug/cjson_rust.d
@@ -0,0 +1,1 @@
+/home/idawy/cJSON/cjson_rust/target/debug/cjson_rust: /home/idawy/cJSON/cjson_rust/src/main.rs
---
+++ cjson_rust/target/debug/deps/cjson_rust-0c65f3d7bac776e7.d
@@ -0,0 +1,5 @@
+/home/idawy/cJSON/cjson_rust/target/debug/deps/cjson_rust-0c65f3d7bac776e7.d: src/main.rs
+
+/home/idawy/cJSON/cjson_rust/target/debug/deps/cjson_rust-0c65f3d7bac776e7: src/main.rs
+
+src/main.rs:
